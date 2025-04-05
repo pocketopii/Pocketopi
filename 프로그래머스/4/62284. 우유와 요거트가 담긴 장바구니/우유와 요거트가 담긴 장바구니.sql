6c4bf1ad1ee3815064e95efc4076efb1f7071402
pocketopii <rlwns00@gmail.com>
2025-04-05 15:50:43 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT CART_ID
+FROM CART_PRODUCTS
+WHERE CART_ID IN (SELECT CART_ID
+                 FROM CART_PRODUCTS
+                 WHERE NAME = 'Milk')
+                 AND NAME = 'Yogurt'
+ORDER BY 1
